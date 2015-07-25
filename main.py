34e962b4f3f4182e39ee0ec1bec9c20cc3e15cd4
brett blashko <blashko.brett@gmail.com>
2015-07-25 17:22:11 +0000

    created function parseFile

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello World!');
+import os
+import sys
+import string
+
+#-----parseFile(filename)-----#
+#Purpose: encodes input file into one string
+def parseFile(filename):
+
+    try:
+        open_file = open(filename)
+    except:
+        print("unable to open file: " + filename)
+
+    replaceCharacters = ['.', '*', '?']
+    content = open_file.readlines()
+    encodedLine = "";
+    for line in content:
+        encodedLine += ''.join(line.split())
+
+    encodedLine = encodedLine.replace('.', '0').replace('*', '0').replace('?', '0')
+    return encodedLine
+
+
+#-----MAIN-----#
+line = parseFile(sys.argv[1])
+print("\nencodedLine = " + line)
