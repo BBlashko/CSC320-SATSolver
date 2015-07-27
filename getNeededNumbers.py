a59cb780991f5ac6e9b34e180a9bd353f854f279
brett blashko <blashko.brett@gmail.com>
2015-07-27 18:47:34 +0000

    added some comments

--- getNeededNumbers.py
+++ getNeededNumbers.py
@@ -1,7 +1,14 @@
+
+#-----getNeededNumbers(sector)-----#
+#Purpose: Given a single column, row, 9x9 box, determine what numbers are needed
+#         outputs needed number into a string of 0's and 1's if number needed location in string is a 1
+#         EG. returns "000011001" numbers needed are 5, 6, 8
 def getNeededNumbers(sector):
   neededNumbers = ['1', '1', '1', '1', '1', '1', '1', '1', '1',]
 
   print(sector)
+  #for each character in sector determine if number is present. if so replace
+  #numbers corresponding location with a '0'
   for character in sector:
     if(character == '0'):
       neededNumbers[0] = neededNumbers[0].replace('1', '0')
@@ -22,7 +29,7 @@ def getNeededNumbers(sector):
     elif(character == '9'):
       neededNumbers[8] = neededNumbers[8].replace('1', '0')
 
-
+  #convert neededNumbers back into a string and return
   neededNumbers = "".join(neededNumbers)
   print(neededNumbers)
   return neededNumbers;
