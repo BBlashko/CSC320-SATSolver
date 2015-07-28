444f78671b30b6ea8dd6b7215a3187be0c3a1a94
d-parmar <deepak-parmar@hotmail.ca>
2015-07-28 13:29:07 -0700

    Update methodCNF.py

--- methodCNF.py
+++ methodCNF.py
@@ -1,3 +1,7 @@
+#Import the needed numbers to find which number is required for each row in the grid.
+
+from getNeededNumbers import getNeededNumbers
+
 #Generate the rows 
 def generateRowCNF(grid):
 	rows_list = []
@@ -5,8 +9,11 @@ def generateRowCNF(grid):
 	rows = []
 		for x in range(GRID):
 			rows.append(grid[x][y])
-			rows_list.append(rows)
-	return rows_list
+	# Find the needed number for each of the rows, and once that is done, append the result of the rows into rows_list		
+	rows.append(getNeededNumbers(rows))
+    	rows_list.append(rows)
+    	
+return rows_list
 
 def generateColumnCNF(grid):
   return 1
