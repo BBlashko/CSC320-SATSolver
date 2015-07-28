dcb8cc0240d7c9a19892a2a9847c81f79379319b
Brett Binnersley <brettbinnersley14@gmail.com>
2015-07-28 13:48:33 -0700

    Fixed merge conflicts and errors

--- methodCNF.py
+++ methodCNF.py
@@ -22,9 +22,9 @@ def generateColumnCNF(grid):
 
 def generate3X3CNF(grid):
   #Turn the grid into
-  for gridX in range(3)
-    for gridY in range(3)
-      for x in range(3)
-        for y in range(3)
+  for gridX in range(3):
+    for gridY in range(3):
+      for x in range(3):
+        for y in range(3):
           xpos = gridX * 3 + x
           ypos = gridY * 3 + y
